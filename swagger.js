c9fa1b1c9f872c3887738227e4bba0f5c66aceab
AudreyChecketts <121894300+achkts@users.noreply.github.com>
2025-02-01 23:53:19 -0700

    commet out host and port

--- swagger.js
+++ swagger.js
@@ -1,8 +1,8 @@
 const swaggerAutogen = require('swagger-autogen')();
 const dotenv = require('dotenv');
 dotenv.config();
-const port = process.env.PORT
-const host = process.env.HOST
+// const port = process.env.PORT
+// const host = process.env.HOST
 
 const doc = {
   info: {
